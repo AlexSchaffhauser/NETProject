2883035cde21ceaed4c2c14c1f8611ae70fdafde
Alex <alexschaffhauser@gmail.com>
2020-01-31 16:00:12 -0500

    Changes made in all forms
    Work to be done: Refresh Form1 from Form2. Select only buildings belonging to selected planet in Form3. Add the rest of Form3's functionality.

---
+++ DotNETGalaxyView/Form2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace DotNETGalaxyView
+{
+    public partial class Form2 : Form
+    {
+        public string incomingForm2Name;
+        BuildingContext _context;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            _context = new BuildingContext();
+            // Call the Load method to get the data for the given DbSet
+            // from the database.
+            // The data is materialized as entities. The entities are managed by
+            // the DbContext instance.
+            _context.Planets.Load();
+            // Bind the categoryBindingSource.DataSource to
+            // all the Unchanged, Modified and Added Category objects that
+            // are currently tracked by the DbContext.
+            // Note that we need to call ToBindingList() on the
+            // ObservableCollection<TEntity> returned by
+            // the DbSet.Local property to get the BindingList<T>
+            // in order to facilitate two-way binding in WinForms.
+            this.planetBindingSource.DataSource = _context.Planets.Local.ToBindingList();
+        }
+
+        private void btnAddPlanet_Click(object sender, EventArgs e)
+        {
+            //add new Planet
+            var newPlanet = new Planet()
+            {
+                PlanetName = tbxPlanetName.Text
+            };
+            _context.Planets.Add(newPlanet);
+            //Add Factory to Planet
+            var factory = new Building()
+            {
+                BuildingName = "Factory",
+                BuildingCost = 10,
+                BuildingLevel = 1,
+                BuildingId = newPlanet.PlanetId
+            };
+            _context.Buildings.Add(factory);
+            //Add Storage to Planet
+            var storage = new Building()
+            {
+                BuildingName = "Storage",
+                BuildingCost = 10,
+                BuildingLevel = 1,
+                BuildingId = newPlanet.PlanetId
+            };
+            _context.Buildings.Add(storage);
+            _context.SaveChanges();
+            this.Close();
+        }
+
+    }
+}
---
+++ DotNETGalaxyView/Form3.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace DotNETGalaxyView
+{
+    public partial class Form3 : Form
+    {
+        BuildingContext _context;
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            _context = new BuildingContext();
+            // Call the Load method to get the data for the given DbSet
+            // from the database.
+            // The data is materialized as entities. The entities are managed by
+            // the DbContext instance.
+            _context.Buildings.Load();
+            // Bind the categoryBindingSource.DataSource to
+            // all the Unchanged, Modified and Added Category objects that
+            // are currently tracked by the DbContext.
+            // Note that we need to call ToBindingList() on the
+            // ObservableCollection<TEntity> returned by
+            // the DbSet.Local property to get the BindingList<T>
+            // in order to facilitate two-way binding in WinForms.
+            this.buildingsBindingSource.DataSource = _context.Buildings.Local.ToBindingList();
+        }
+
+        private void btn_Upgrade_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buildingsDataGridView_DefaultValuesNeeded(object sender, System.Windows.Forms.DataGridViewRowEventArgs e)
+        {
+            //e.Row.Cells["Name"].Value = "Factory";
+            //e.Row.Cells["Cost"].Value = 10;
+            //e.Row.Cells["Level"].Value = 1;
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+            planetNameTextBox.Text = Form1.sendingForm3Name;
+            planetResourcesTextBox.Text = Form1.sendingForm3Resources.ToString();
+        }
+    }
+}
